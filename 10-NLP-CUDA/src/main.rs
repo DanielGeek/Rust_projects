9ec30276d95dae53067b8b4d2f5abae25ec36233
Daniel <daniel.passage606@passmail.com>
2024-11-22 23:55:25 -0400

    Added translation function and bert NLP crate

--- 10-NLP-CUDA/src/main.rs
+++ 10-NLP-CUDA/src/main.rs
@@ -1,5 +1,32 @@
+use rust_bert::pipelines::{common::ModelType, translation::{Language, TranslationModelBuilder}};
+use tch::Device;
+
 
 
 fn main() {
-  println!("Hello!");
+  translation();
+}
+
+fn translation() {
+  let model = TranslationModelBuilder::new()
+      .with_device(Device::cuda_if_available())
+      .with_model_type(ModelType::Marian)
+      .with_source_languages(vec![Language::English])
+      .with_target_languages(vec![Language::Spanish])
+      .create_model()
+      .unwrap();
+
+  let text = "Hello, How are you? Please Subscribe!";
+
+  let output = model
+      .translate(
+        &[text],
+        None,
+        Language::Spanish
+      )
+      .unwrap();
+
+    for sentence in output {
+      println!("{sentence}");
+    }
 }
