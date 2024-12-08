ac580b3e08b7078ec0af55f7c724ee228d8d6520
Daniel <daniel.passage606@passmail.com>
2024-12-07 22:18:33 -0400

    CRUD App | Part - 2 | Add Service Layer & Connect to DB

--- 12-crud-app/src/controller.rs
+++ 12-crud-app/src/controller.rs
@@ -1,8 +1,9 @@
-use crate::model::User;
 use axum::extract::Path;
 use axum::http::StatusCode;
 use axum::Json;
 use serde_json::Value;
+
+use crate::model::{User, UserInfo};
 
 pub async fn list_users() -> (StatusCode, Json<Value>) {
     // get users
@@ -12,11 +13,11 @@ pub async fn get_user_by_id(Path(id): Path<u64>) -> (StatusCode, Json<Value>) {
     // get user by id
 }
 
-pub async fn create_user(Json(user): Json<User>) -> StatusCode {
+pub async fn create_user(Json(user): Json<UserInfo>) -> StatusCode {
     // create user
 }
 
-pub async fn update_user(Path(id): Path<u64>, Json(user): Json<User>) -> StatusCode {
+pub async fn update_user(Path(id): Path<u64>, Json(user): Json<UserInfo>) -> StatusCode {
     // update user
 }
 
