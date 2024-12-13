fa2d99c2cc57f0657c6b87ab17e2ae2a6cb13fe1
Daniel <daniel.passage606@passmail.com>
2024-12-13 17:58:43 -0400

    Step 3 - Once

--- 14-singleton/src/lib.rs
+++ 14-singleton/src/lib.rs
@@ -71,3 +71,55 @@ mod step2 {
         }
     }
 }
+
+mod step3 {
+    use std::sync::{Mutex, Once};
+
+    pub static SINGLETON: Mutex<Singleton> = Mutex::new(Singleton::new(|| 0));
+    pub static ONCE: Once = Once::new();
+
+    pub struct Singleton {
+        inner: Option<u32>,
+        init: fn() -> u32,
+    }
+
+    impl Singleton {
+        pub const fn new(init: fn() -> u32) -> Self {
+            Singleton { inner: None, init }
+        }
+
+        pub fn instance() -> *mut u32 {
+            ONCE.call_once(|| {
+                let init = SINGLETON.lock().unwrap().init;
+                let value = init();
+                SINGLETON.lock().unwrap().inner = Some(value);
+                println!("Initializing singleton value");
+            });
+
+            println!("Accessing singleton value");
+
+            let mut singleton = SINGLETON.lock().unwrap();
+            singleton.inner.as_mut().map_or_else(
+                || std::ptr::null_mut(),
+                |inner| inner as *mut u32,
+            )
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn singleton() {
+            unsafe {
+                let instance1 = Singleton::instance();
+                let instance2 = Singleton::instance();
+                assert_eq!(*instance1, *instance2);
+
+                *instance1 = 42;
+                assert_eq!(*instance1, *instance2);
+            }
+        }
+    }
+}
