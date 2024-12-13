50eac1ba262fde52c42dbccbcff1d8c04d871c2c
Daniel <daniel.passage606@passmail.com>
2024-12-13 17:36:27 -0400

    Step 2 - Using Option

--- 14-singleton/src/lib.rs
+++ 14-singleton/src/lib.rs
@@ -32,3 +32,42 @@ mod step1 {
         }
     }
 }
+
+mod step2 {
+    pub static mut SINGLETON: Singleton = Singleton::new();
+
+    pub struct Singleton {
+        inner: Option<u32>,
+    }
+
+    impl Singleton {
+        pub const fn new() -> Self {
+            Singleton { inner: None }
+        }
+
+        fn init(&mut self, value: u32) {
+            self.inner = Some(value);
+        }
+
+        pub fn instance() -> *mut Option<u32> {
+            unsafe { &raw mut SINGLETON.inner }
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn singleton() {
+            unsafe {
+                let instance1 = Singleton::instance();
+                let instance2 = &raw mut SINGLETON;
+
+                *instance1 = Some(42);
+
+                assert_eq!(*instance1, (*instance2).inner);
+            }
+        }
+    }
+}
