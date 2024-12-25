37807f9a15b28fa96ae5b51b62041935c0db0eb4
Daniel <daniel.passage606@passmail.com>
2024-12-24 21:10:48 -0400

    Add CORS Middleware

--- 20-axum/lessons/routing/src/routes/mod.rs
+++ 20-axum/lessons/routing/src/routes/mod.rs
@@ -7,9 +7,7 @@ mod path_variables;
 mod query_params;
 
 use axum::{
-    body::Body,
-    routing::{get, patch, post},
-    Router,
+    body::Body, http::Method, routing::{get, patch, post}, Router
 };
 use hello_world::hello_world;
 use mirror_body_json::mirror_body_json;
@@ -18,8 +16,13 @@ use mirror_user_agent::mirror_user_agent;
 use path_variables::{path_variables, hard_coded_path};
 use query_params::query_params;
 use mirror_custom_header::mirror_custom_header;
+use tower_http::cors::{Any, CorsLayer};
 
 pub fn create_routes() -> Router<Body> {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
+
     Router::new()
         .route("/", patch(hello_world))
         .route("/mirror_body_string", post(mirror_body_string))
@@ -29,4 +32,5 @@ pub fn create_routes() -> Router<Body> {
         .route("/query_params", get(query_params))
         .route("/mirror_user_agent", get(mirror_user_agent))
         .route("/mirror_custom_header", get(mirror_custom_header))
+        .layer(cors)
 }
