93d1fd3aaaaaaa9e94d4912e895afeabdb96f772
Daniel <daniel.passage606@passmail.com>
2024-12-27 19:27:52 -0400

    Add Soft Deleting Data

--- 20-axum/lessons/data/src/routes/delete_task.rs
+++ 20-axum/lessons/data/src/routes/delete_task.rs
@@ -1,10 +1,17 @@
 use crate::database::tasks::{self, Entity as Tasks};
-use axum::{extract::Path, http::StatusCode, Extension};
-use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, IntoActiveModel, QueryFilter};
+use axum::{extract::{Path, Query}, http::StatusCode, Extension};
+use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, IntoActiveModel, QueryFilter, Set};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct QueryParams {
+    soft: bool,
+}
 
 pub async fn delete_task(
     Path(task_id): Path<i32>,
     Extension(database): Extension<DatabaseConnection>,
+    Query(query_params): Query<QueryParams>,
 ) -> Result<(), StatusCode> {
     // DELETE TASKS OPTION 1
     // let task = if let Some(task) = Tasks::find_by_id(task_id)
@@ -27,12 +34,31 @@ pub async fn delete_task(
     //     .await
     //     .map_err(|_error| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    // DELETE TASKS OPTION 3
-    Tasks::delete_many()
-        .filter(tasks::Column::Id.eq(task_id))
-        .exec(&database)
-        .await
-        .map_err(|_error| StatusCode::INTERNAL_SERVER_ERROR)?;
+    if query_params.soft {
+        let mut task = if let Some(task) = Tasks::find_by_id(task_id)
+            .one(&database)
+            .await
+            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)? {
+                task.into_active_model()
+            } else {
+                return Err(StatusCode::NOT_FOUND);
+            };
+
+            let now = chrono::Utc::now();
+
+            task.deleted_at = Set(Some(now.into()));
+            Tasks::update(task)
+                .exec(&database)
+                .await
+                .map_err(|_error| StatusCode::INTERNAL_SERVER_ERROR)?;
+    } else {
+        // DELETE TASKS OPTION 3
+        Tasks::delete_many()
+            .filter(tasks::Column::Id.eq(task_id))
+            .exec(&database)
+            .await
+            .map_err(|_error| StatusCode::INTERNAL_SERVER_ERROR)?;
+    }
 
     Ok(())
 }
