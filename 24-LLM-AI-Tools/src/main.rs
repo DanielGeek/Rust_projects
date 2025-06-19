1eb81a70cd2f23a24f56fd82f1881ef0a0a80332
Daniel Angel <daniel30081990@gmail.com>
2025-06-19 10:30:30 -0400

    Add get_weather tool and update dependencies
    Introduced a new async get_weather tool function using reqwest to fetch weather data for a given city. Updated dependencies in Cargo.toml to specific versions for reqwest, schemars, serde, and tokio. Modified main.rs to use the new get_weather tool instead of Calculator, and updated the model name in Coordinator initialization.

--- 24-LLM-AI-Tools/src/main.rs
+++ 24-LLM-AI-Tools/src/main.rs
@@ -1,24 +1,37 @@
-use ollama_rs::{coordinator::Coordinator, generation::{chat::ChatMessage, tools::implementations::Calculator}, Ollama};
+use ollama_rs::{
+    Ollama,
+    coordinator::Coordinator,
+    generation::{chat::ChatMessage, tools::implementations::Calculator},
+};
+
+/// Get the weather for a given city.
+///
+/// * city - City to get the weather for.
+#[ollama_rs::function]
+async fn get_weather(city: String) -> Result<String, Box<dyn Error + Sync + Send>> {
+    print!("Inside tool ");
+    Ok(reqwest::get(format!("https://wttr.in/{city}?format=%C+%t"))
+        .await?
+        .text()
+        .await?)
+}
 
 #[tokio::main]
 async fn main() {
     let ollama = Ollama::default();
     let history = vec![];
 
-    let tools = ollama_rs::tool_group![Calculator {}];
+    let tools = ollama_rs::tool_group![get_weather];
+    // let tools = ollama_rs::tool_group![Calculator {}];
 
-    let mut cordinator = Coordinator::new_with_tools(ollama,
-        String::from("llama3.2:1b"),
-        history,
-        tools);
+    let mut cordinator =
+        Coordinator::new_with_tools(ollama, String::from("llama3.2:latest"), history, tools);
 
-    let user_message = "2+2*2-4";
+    let user_message = "What's the weather in Zulia?";
 
     let user_chat_message = ChatMessage::user(user_message.to_owned());
 
     let resp = cordinator.chat(vec![user_chat_message]).await.unwrap();
 
     print!("Response: {}", resp.message.content);
-
-    
 }
