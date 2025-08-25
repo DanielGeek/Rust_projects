f8bf44695aa2e38715f525ff8f6d59ca867280c7
Daniel Ángel Barreto <daniel30081990@gmail.com>
2025-08-25 00:36:34 -0400

    Fix ownership exercises to avoid move errors
    Updated solutions in ownership exercises to use cloning instead of moving String values. This allows variables to be used after assignment or function calls without ownership errors.

--- 33-rust-crash-course/topics/ownership/exercises/src/lib.rs
+++ 33-rust-crash-course/topics/ownership/exercises/src/lib.rs
@@ -1,14 +1,14 @@
 pub fn exercise_1() {
     let s = "rust".to_string();
-    let s1 = s;
-    let s2 = s;
+    let s1 = s.clone();
+    let _s2 = s.clone();
     println!("{s1}");
 }
 
 pub fn exercise_2() {
     let s = "rust".to_string();
     {
-        let s1 = s;
+        let s1 = s.clone();
         println!("{s1}");
     }
     println!("{s}");
@@ -20,7 +20,7 @@ fn take(s: String) {
 
 pub fn exercise_3() {
     let s = "rust".to_string();
-    take(s);
+    take(s.clone());
     println!("{s}");
     println!("{s}");
 }
--- 33-rust-crash-course/topics/ownership/solutions/src/lib.rs
+++ 33-rust-crash-course/topics/ownership/solutions/src/lib.rs
@@ -1,17 +1,17 @@
 pub fn exercise_1() {
     let s = "rust".to_string();
-    let s1 = s;
-    // let s2 = s;
+    let s1 = s.clone();
+    let s2 = s;
     println!("{s1}");
 }
 
 pub fn exercise_2() {
     let s = "rust".to_string();
     {
-        let s1 = s;
+        let s1 = s.clone();
         println!("{s1}");
     }
-    // println!("{s}");
+    println!("{s}");
 }
 
 fn take(s: String) {
@@ -20,7 +20,7 @@ fn take(s: String) {
 
 pub fn exercise_3() {
     let s = "rust".to_string();
-    // take(s);
+    take(s.clone());
     println!("{s}");
     println!("{s}");
 }
