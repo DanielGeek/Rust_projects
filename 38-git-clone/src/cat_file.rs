f41b8e8237446d8798a191ecaf943dd55b166911
Daniel Ángel Barreto <daniel30081990@gmail.com>
2025-09-23 14:28:44 -0400

    created cat_file and init modules

---
+++ 38-git-clone/src/cat_file.rs
@@ -0,0 +1,23 @@
+use std::fs;
+use flate2::read::ZlibDecoder;
+use std::io::Read;
+
+pub fn cat_file(args: &[String]) {}
+    
+pub fn pretty_print(hash: String) {
+    // the first two characters of the hash are the folder name, and the rest are the file name
+    let folder_name = &hash[0..2];
+    let file_name = &hash[2..];
+    let path = format!(".git/objects/{folder_name}/{file_name}");
+    let mut object = fs::File::open(&path).unwrap();
+    let mut content: Vec<u8> = vec![];
+    let mut extracted_content = String::new();
+
+    object.read_to_end(&mut content).unwrap();
+
+    let mut decoder = ZlibDecoder::new(content.as_slice());
+
+    decoder.read_to_string(&mut extracted_content).unwrap();
+
+    print!("{extracted_content}");
+}
--- 38-git-clone/src/main.rs
+++ 38-git-clone/src/main.rs
@@ -1,8 +1,8 @@
+use codecrafters_git::{
+    cat_file::{cat_file, pretty_print},
+    init::init,
+};
 use std::env;
-use std::fs;
-use std::io::Read;
-
-use flate2::bufread::ZlibDecoder;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,32 +10,9 @@ fn main() {
 
     // Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-        println!("Initialized git directory")
-    } else if args[1] == "cat-file" {
-        if args[2] == "-p" {
-            let hash = args[3].as_str();
-            // the first two characters of the hash are the folder name, and the rest are the file name
-            let folder_name = &hash[0..2];
-            let file_name = &hash[2..];
-            let path = format!(".git/objects/{folder_name}/{file_name}");
-            let mut object = fs::File::open(&path).unwrap();
-            let mut content: Vec<u8> = vec![];
-            let mut extracted_content = String::new();
-
-            object.read_to_end(&mut content).unwrap();
-
-            let mut decoder = ZlibDecoder::new(content.as_slice());
-
-            decoder.read_to_string(&mut extracted_content).unwrap();
-
-            println!("{extracted_content}");
-        }
-    } else {
-        println!("unknown command: {}", args[1]);
+    match args[1].as_str() {
+        "init" => init(),
+        "cat-file" => cat_file(&args[2..]),
+        _ => println!("unknown command: {}", args[1]),
     }
 }
